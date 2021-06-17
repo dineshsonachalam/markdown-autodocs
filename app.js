16e6ce1a5a1b71249fb662414e9e723927828b9c
dineshsonachalam <dineshsonachalam@gmail.com>
2021-06-17 13:22:36 +0530

    Code cleanup

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ import markdownMagic from 'markdown-magic'
 import {markdownTable} from 'markdown-table'
 import githubApi from './githubApi.js'
 
+export const app = async function(repo, branch, githubApiToken) {
+    const github = new githubApi(repo, branch, githubApiToken)
+    const workflowNames = await github.getWorkflowNames()
+    const workflowIds   = await github.getWorkflowIds(workflowNames)
+    const workflowArtifacts = await github.getWorkflowArtifacts(workflowIds)
+    console.log("workflowArtifacts: ", workflowArtifacts)
+}
 
 
 
 
 
-let repo = process.env.repo;
-let branchInfo = process.env.branch.split("/")
-let branch  = branchInfo[branchInfo.length - 1]
-let githubApiToken = process.env.githubApiToken
-const github = new githubApi(repo, branch, githubApiToken)
-const workflowNames = await github.getWorkflowNames()
-const workflowIds   = await github.getWorkflowIds(workflowNames)
-const workflowArtifacts = await github.getWorkflowArtifacts(workflowIds)
+
+
 

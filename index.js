b5116acd6c772bffdca501c92794126466dffd25
dineshsonachalam <dineshsonachalam@gmail.com>
2021-06-18 06:20:17 +0530

    Code cleanup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import pkg from 'log4js';
 import {app} from './app.js'
 import {Command} from 'commander/esm.mjs';
 const program = new Command();
@@ -6,9 +7,13 @@ program
   .option('-b, --branch <type>', 'Branch name')
   .option('-a, --accessToken <type>', 'Github Access token')
   .option('-i, --inputFilePath <type>', 'Input file path')
-
 program.parse(process.argv)
 const options = program.opts()
+const { getLogger } = pkg;
+const logger = getLogger();
+logger.level = "info";
+
+
 if(options.repo && options.branch && options.accessToken && options.inputFilePath){
   let repo = options.repo
   let branchInfo  = options.branch
@@ -22,10 +27,10 @@ if(options.repo && options.branch && options.accessToken && options.inputFilePat
   let accessToken = options.accessToken
   let inputFilePath = options.inputFilePath  
   const message = await app(inputFilePath, repo, branch, accessToken)
-  console.log(message)
+  logger.info(message)
 }else{
-  console.log("Missed arguments. Please make sure if you missed any arguments.")
-  console.log("This CLI requires repo, branch, accessToken, inputFilePath arguments")
+  logger.info("Missed arguments. Please make sure if you missed any arguments.")
+  logger.info("This CLI requires repo, branch, accessToken, inputFilePath arguments")
 }
 
 // node index.js --repo $repo --branch $branch --accessToken $accessToken --inputFilePath ./readme.md

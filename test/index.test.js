ff0763ba102fa936e711265d17080f9c158e1cc7
dineshsonachalam <dineshsonachalam@gmail.com>
2021-06-19 13:05:20 +0530

    code cleanup

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,7 @@ describe('Validate autodoc-workflow-artifacts CLI', () => {
   beforeEach(() => {
     jest.setTimeout(25000);
   });
+  
   it('Gave all the required arguments to the CLI as an input, it should now return a success message', (done) => {
     exec(
       'node index.js --repo $TEST_REPO --branch $TEST_BRANCH --accessToken $TEST_ACCESSTOKEN --inputFilePath ./readme.md',
@@ -14,6 +15,7 @@ describe('Validate autodoc-workflow-artifacts CLI', () => {
   });
 
   it('Missed required arguments to the CLI as an input, it should now return a failure message', (done) => {
+    jest.setTimeout(15000);
     exec(
       'node index.js --repo $repo --branch $branch --accessToken $accessToken',
       (error, stdout, stderr) => {

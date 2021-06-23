b99ac59bd538cd98991c1569fa837e07861aedf2
dineshsonachalam <dineshsonachalam@gmail.com>
2021-06-23 20:32:39 +0530

    code cleanup

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import argparse
 import logging
+import os
 
 def get_cli_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-repo', required=True)
+    parser.add_argument('-access_token', required=True)
     parser.add_argument('-commit_author', required=True)
     parser.add_argument('-commit_user_email', required=True)
     parser.add_argument('-commit_message', required=True)
@@ -20,12 +23,16 @@ def option_processor(option):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     args = get_cli_args()
+    repo = args.repo
+    access_token = args.access_token
     commit_author = args.commit_author
     commit_user_email = args.commit_user_email
     commit_message = args.commit_message
     branch = args.branch
     output_file_paths = option_processor(args.output_file_paths)
     categories = option_processor(args.categories)
+    print("repo: ",repo)
+    print("access_token: ", access_token)
     print("commit_author: ", commit_author)
     print("commit_user_email: ", commit_user_email)
     print("commit_message: ", commit_message)
@@ -33,7 +40,7 @@ if __name__ == "__main__":
     print("output_file_paths: ", output_file_paths)
     print("categories: ", categories)
 
-# python3 app.py -commit_author 'dineshsonachalam' -commit_user_email 'dineshsonachalam@gmail.com' -commit_message 'Apply automatic changes' -branch 'master' -output_file_paths '[./README.md,./docs/Intro.md]' -categories '[code-block,json-to-html-table,workflow-artifact-table]'    
+# python3 app.py -repo $repo -access_token $accessToken -commit_author 'dineshsonachalam' -commit_user_email 'dineshsonachalam@gmail.com' -commit_message 'Apply automatic changes' -branch 'master' -output_file_paths '[./README.md]' -categories '[code-block,json-to-html-table,workflow-artifact-table]'    
 # commit_author:  dineshsonachalam
 # commit_user_email:  dineshsonachalam@gmail.com
 # commit_message:  Apply automatic changes

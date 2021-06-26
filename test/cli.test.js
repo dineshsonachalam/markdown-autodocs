594685e3f950f82b2303f0e2824f91779d89bcf0
dineshsonachalam <dineshsonachalam@gmail.com>
2021-06-26 08:20:29 +0530

    code cleanup

--- test/cli.test.js
+++ test/cli.test.js
@@ -2,7 +2,17 @@ const { exec } = require('child_process');
 describe('Validate markdown-autodocs CLI', () => {
   
   beforeEach(() => {
-    jest.setTimeout(25000);
+    jest.setTimeout(60000);
+  });
+  
+  it('Autodoc workflow artifact table. Gave all the required arguments to the CLI as an input, it should now return a success message', (done) => {
+    exec(
+      'node index.js -c workflow-artifact-table -o ./README.md -r $TEST_REPO -b $TEST_BRANCH -a $TEST_ACCESSTOKEN',
+      (error, stdout, stderr) => {
+        expect(stdout).toMatch(/([INFO] default - Added|artifacts)/i)
+        done();
+      }
+    );
   });
   
   it('Autodoc code block. Gave all the required arguments to the CLI as an input, it should now return a success message', (done) => {
@@ -20,16 +30,6 @@ describe('Validate markdown-autodocs CLI', () => {
       'node index.js -c json-to-html-table -o ./README.md -r $TEST_REPO -b $TEST_BRANCH -a $TEST_ACCESSTOKEN',
       (error, stdout, stderr) => {
         expect(stdout).toMatch(/(Converted JSON to HTML table.)/i)
-        done();
-      }
-    );
-  });
-
-  it('Autodoc workflow artifact table. Gave all the required arguments to the CLI as an input, it should now return a success message', (done) => {
-    exec(
-      'node index.js -c workflow-artifact-table -o ./README.md -r $TEST_REPO -b $TEST_BRANCH -a $TEST_ACCESSTOKEN',
-      (error, stdout, stderr) => {
-        expect(stdout).toMatch(/([INFO] default - Added|artifacts)/i)
         done();
       }
     );

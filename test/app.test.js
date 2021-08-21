35382362bd572e4ae7782cda54164137893dbe28
dineshsonachalam <dineshsonachalam@gmail.com>
2021-08-21 10:41:54 +0530

    code cleanup

--- test/app.test.js
+++ test/app.test.js
@@ -9,11 +9,6 @@ describe("should test App functionality", () => {
     const branch = process.env.TEST_BRANCH;
     const githubApiToken = process.env.TEST_ACCESSTOKEN;
     
-    child.exec(
-        "wget -c -N  https://gist.githubusercontent.com/dineshsonachalam/17965bed1d67ea51ee9c80473f70f078/raw/86fa6b562af89c39113e1cde66ab15c6cf0c7ac8/TEST_README.md https://gist.githubusercontent.com/dineshsonachalam/cb143249e4daeb847992577da2f8e2db/raw/d53d1c8d16a58d039f64e2b21218ba3e2464d063/sponsors.json https://gist.githubusercontent.com/dineshsonachalam/db8cec9c076d2a423b28a09d105cd8c6/raw/52865d53575287adbdf203c03acd705c69938a6a/contributors.json",
-        (error, stdout, stderr) => output(error, stdout, stderr)
-    );
-    
     test("should test code-block", async () => {
         expect(await app("./TEST_README.md", "code-block", repo, branch, githubApiToken))
         .toMatch("Auto documented code-block in ./TEST_README.md");

edc7c7c132def47a30e802158e455d38a505cb49
Dinesh Sonachalam <dineshsonachalam@gmail.com>
2022-08-29 20:45:16 +0100

    Update action.py

--- action.py
+++ action.py
@@ -19,7 +19,7 @@ class GithubAction:
         self.commit_author = cli_args.commit_author
         self.commit_user_email = cli_args.commit_user_email
         self.commit_message = cli_args.commit_message
-        self.branch = os.environ['GITHUB_REF_NAME']
+        self.branch = os.environ['GITHUB_HEAD_REF']
         self.output_file_paths = cli_args.output_file_paths
         self.categories = cli_args.categories
 

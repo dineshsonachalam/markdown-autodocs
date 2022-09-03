e7e8c7a7fae170d4ecac33b6c45d556233652e4f
Dinesh Sonachalam <dineshsonachalam@Dineshs-MacBook-Air.local>
2022-09-03 16:20:55 +0100

    Added Docker

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:current-alpine3.16
+WORKDIR /usr/src/app
+RUN npm i -g doctoc@2.1.0 markdown-autodocs
+ENTRYPOINT ["markdown-autodocs"]
+CMD ["--version"]
--- action.py
+++ action.py
@@ -52,10 +52,12 @@ class GithubAction:
         self.get_output_file_paths()
         self.get_categories()
         ma_cli_command = f"markdown-autodocs --outputFilePath {self.output_file_paths} --category {self.categories} --repo {self.repo} --branch {self.branch} --accessToken {self.access_token}"
-        print(f"ma_cli_command: {ma_cli_command}")
-        os.system("sudo npm i -g doctoc@2.1.0")
-        os.system("sudo npm i -g markdown-autodocs")
-        os.system(ma_cli_command)
+        os.system("docker build -t markdown-autodocs:latest .")
+        os.system(f"docker run {ma_cli_command}")
+        # print(f"ma_cli_command: {ma_cli_command}")
+        # os.system("sudo npm i -g doctoc@2.1.0")
+        # os.system("sudo npm i -g markdown-autodocs")
+        # os.system(ma_cli_command)
         
     def autodocument_markdown_files(self):
         self.start_markdown_autodocs()

994ab8d4a997a1ea5b4227e53588ca7a0ed51e15
Dinesh Sonachalam <dineshsonachalam@gmail.com>
2022-09-03 19:23:35 +0100

    Update action.py

--- action.py
+++ action.py
@@ -54,7 +54,7 @@ class GithubAction:
         docker_image = "dineshsonachalam/markdown-autodocs"
         options = f"--outputFilePath {self.output_file_paths} --category {self.categories} --repo {self.repo} --branch {self.branch} --accessToken {self.access_token}"
         os.system(f"docker pull {docker_image}:latest")
-        os.system(f"docker run {docker_image} {options}")
+        os.system(f"docker run -v $(pwd):/usr/src/app {docker_image} {options}")
         
     def autodocument_markdown_files(self):
         self.start_markdown_autodocs()
